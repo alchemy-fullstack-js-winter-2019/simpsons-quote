de300580cc7a0c0fb9a0cdb767b8637b0aaa2de0
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-08 16:40:39 -0800

    Added services Api test & passing

---
+++ src/services/simpsonsApi.test.js
@@ -0,0 +1,20 @@
+const { getQuote } = require('./simpsonsApi');
+
+jest.mock('./simpsonsApi');
+
+describe('Simpsons Api Service test', () => {
+  it('gets a quote from Api', () => {
+    return getQuote()
+      .then(quote => {
+        expect(quote).toEqual([
+          {
+            'quote': 'For once maybe someone will call me "sir" without adding, "You\'re making a scene."',
+            'character': 'Homer Simpson',
+            'image': 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939',
+            'characterDirection': 'Right'
+          }
+        ]);
+      });
+  });
+});
+
