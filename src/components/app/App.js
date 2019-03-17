19735d27e088591bfe4e81003b7aa6850b30f6ca
Sophie Graham <sophiergraham1@gmail.com>
2019-03-16 20:07:40 -0700

    button showing up on page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SimpsonQuote from '../../containers/quotes/SimpsonQuote';
+import Load from '../../components/quote/Load';
 
 export default function App() {
   return (
     <>
-      <h1>Simpson Quote</h1>
+      <h1>Simpsons Quote</h1>
       <SimpsonQuote />
+      <Load />
     </>
   );
 }
