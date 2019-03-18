a19444deb74a0f6b913f325dfb3da00b0c5a90ef
Sophie Graham <sophiergraham1@gmail.com>
2019-03-18 00:58:52 -0700

    fixed the button, now only one is showing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import SimpsonQuote from '../../containers/quotes/SimpsonQuote';
-import Load from '../../components/quote/Load';
 
 export default function App() {
   return (
     <>
       <h1>Simpsons Quote</h1>
       <SimpsonQuote />
-      <Load />
     </>
   );
 }
--- src/containers/quotes/SimpsonQuote.js
+++ src/containers/quotes/SimpsonQuote.js
@@ -1,10 +1,32 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Quote from '../../components/quote/Quote';
-// import Load from '../../components/quote/Load';
+import Load from '../../components/quote/Load';
 import { fetchQuote } from '../../actions/simpsons';
 import { getQuote, getCharacterName, getCharacterImage, isLoading } from '../../selectors/simpsons';
-import { withFetch } from '../../components/withFetch';
 
+class SimpsonQuote extends PureComponent {
+  static propTypes = {
+    quote: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    fetch: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    return (
+      <>
+        <Quote {...this.props} />
+        <Load {...this.props} />
+      </>
+    );
+  }
+}
 const mapStateToProps = state => ({
   quote: getQuote(state),
   name: getCharacterName(state),
@@ -17,8 +39,6 @@ const mapDispatchToProps = dispatch => ({
     dispatch(fetchQuote());
   }
 });
-
-const SimpsonQuote = withFetch(Quote);
 
 export default connect(
   mapStateToProps,
